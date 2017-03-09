cd82f187f6bb54adcad868d275d6548a34a40110
unknown <dianpertiwi08@gmail.com>
2017-03-09 21:23:03 +0800

    TUGAS 2 DONE

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('DianPHP', function(){
 	echo "DianPHP";
+});
+
+Route::get('hello-world', function(){
+	return 'Hello World';
+});
+
+Route::get('pengguna/{pengguna}', function($pengguna){ //pake parameter ()karena sdh dideklarasikan
+	return "Hello World dari pengguna $pengguna";
+});
+
+
+Route::get('kelasd2/framework/{mhs?}', function($mhs="Anonim"){ //parameter boleh dikasih parameter/engga karena menggunakan tanya?
+	return "Selamat datang $mhs";
 });
