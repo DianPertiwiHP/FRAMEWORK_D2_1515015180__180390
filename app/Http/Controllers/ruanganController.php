7f60afe685c03f69f35600f8d359f0387b1cf348
unknown <dianpertiwi08@gmail.com>
2017-03-24 21:39:17 +0800

    POSTTEST 4

--- app/Http/Controllers/ruanganController.php
+++ app/Http/Controllers/ruanganController.php
@@ -5,23 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use App\ruangan;
+use App\Ruangan;
 
-class ruanganController extends Controller
+class RuanganController extends Controller
 {
-     public function awal()
+    public function awal()
+    {
+      return view('ruangan.awal', ['data'=>Ruangan::all()]);
+    }
+    public function tambah()
+    {
+      return view('ruangan.tambah');
+    }
+  public function simpan(Request $input)
   {
-  	return "Hello dari ruanganController";
-  }
-  public function tambah()
-  {
-  	return $this->simpan();
-  }
-  public function simpan()
-  {
-  	$ruangan = new ruangan();
-  	$ruangan->title = 'Lab Basic Programing';
-  	$ruangan->save();
-  	return "data dengan title {$ruangan->title} telah disimpan";
-  }
+     $ruangan = new Ruangan;
+     $ruangan->title = $input->title;
+     $informasi = $ruangan->save() ? 'Berhasil simpan data' : 'Gagal simpan data';
+       return redirect('ruangan')->with(['informasi'=>$informasi]);
+    }
+     public function edit($id)
+    {
+        $ruangan = Ruangan::find($id);
+        return view('ruangan.edit')->with(array('ruangan'=>$ruangan));
+    }
+    public function lihat($id)
+    {
+        $ruangan = Ruangan::find($id);
+        return view('ruangan.lihat')->with(array('ruangan'=>$ruangan));
+    }
+    public function update($id, Request $input)
+    {
+        $ruangan = Ruangan::find($id);
+        $ruangan->title = $input->title;
+        $informasi = $ruangan->save() ? 'Berhasil update data' : 'Gagal update data';
+        return redirect('ruangan')->with(['informasi'=>$informasi]);
+    }
+    public function hapus($id)
+    {
+        $ruangan = Ruangan::find($id);
+        $informasi = $ruangan->delete() ? 'Berhasil hapus data' : 'Gagal hapus data';
+        return redirect('ruangan')->with(['informasi'=>$informasi]);
+    }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,24 +32,54 @@ Route::get('kelasd2/framework/{mhs?}', function($mhs="Anonim"){ //parameter bole
 	return "Selamat datang $mhs";
 });
 Route::get('pengguna', 'PenggunaController@awal');
-Route::get('pengguna/tambahh', 'PenggunaController@tambah');
+Route::get('pengguna/tambah', 'PenggunaController@tambah');
+Route::get('pengguna/{pengguna}','PenggunaController@lihat');
+Route::post('pengguna/simpan', 'PenggunaController@simpan');
+Route::get('pengguna/edit/{pengguna}', 'PenggunaController@edit');
+Route::post('pengguna/edit/{pengguna}', 'PenggunaController@update');
+Route::get('pengguna/hapus/{pengguna}', 'PenggunaController@hapus');
 
-Route::get('ruangan','ruanganController@awal');
-Route::get('ruangan/tambah','ruanganController@tambah');
+Route::get('ruangan','RuanganController@awal');
+Route::get('ruangan/tambah','RuanganController@tambah');
+Route::get('ruangan/{ruangan}','RuanganController@lihat');
+Route::post('ruangan/simpan','RuanganController@simpan');
+Route::get('ruangan/edit/{ruangan}','RuanganController@edit');
+Route::post('ruangan/edit/{ruangan}','RuanganController@update');
+Route::get('ruangan/hapus/{ruangan}','RuanganController@hapus');
+
+Route::get('matakuliah','MataKuliahController@awal');
+Route::get('matakuliah/tambah','MataKuliahController@tambah');
+Route::get('matakuliah/{matakuliah}','MatakuliahController@lihat');
+Route::post('matakuliah/simpan','MatakuliahController@simpan');
+Route::get('matakuliah/edit/{matakuliah}','MatakuliahController@edit');
+Route::post('matakuliah/edit/{matakuliah}','MatakuliahController@update');
+Route::get('matakuliah/hapus/{matakuliah}','MatakuliahController@hapus');
+
 
 Route::get('dosenn','dosenController@awal');
-Route::get('dosenn/tambah','dosenController@tambah');
+Route::get('dosenn/tambah', 'DosennaController@tambah');
+// Route::post('dosenn/simpan','dosenController@simpan');
+//Route::post('dosenn/edit/{dosenn}','dosenController@update');
+// Route::get('dosenn/hapus/{dosenn}','dosenController@hapus');
 
-Route::get('mahasiswa','mahasiswaController@awal');
-Route::get('mahasiswa/tambah','mahasiswaController@tambah');
+ Route::get('mahasiswa','mahasiswaController@awal');
+ Route::get('mahasiswa/tambah', 'MahasiswaController@tambah');
+// Route::post('mahasiswa/simpan','mahasiswaController@simpan');
+// Route::get('mahasiswa/edit/{mahasiswa}','mahasiswaController@edit');
+// Route::post('mahasiswa/edit/{mahasiswa}','mahasiswaController@update');
+// Route::get('mahasiswa/hapus/{mahasiswa}','mahasiswaController@hapus');
 
-Route::get('matakuliah','matakuliahController@awal');
-Route::get('matakuliah/tambah','matakuliahController@tambah');
+ Route::get('dosen_matakuliah','dosen_matakuliahController@awal');
+ Route::get('dosen_matakuliah/tambah','dosen_matakuliahController@tambah');
+// Route::post('dosen_matakuliah/simpan','dosen_matakuliahController@simpan');
+// Route::get('dosen_matakuliah/edit/{dosen_matakuliah}','dosen_matakuliahController@edit');
+// Route::post('dosen_matakuliah/edit/{dosen_matakuliah}','dosen_matakuliahController@update');
+// Route::get('dosen_matakuliah/hapus/{dosen_matakuliah}','dosen_matakuliahController@hapus');
 
-Route::get('dosen_matakuliah','dosen_matakuliahController@awal');
-Route::get('dosen_matakuliah/tambaah','dosen_matakuliahController@tambaah');
-
-Route::get('jadwal_matakuliah','jadwal_matakuliahController@awal');
-Route::get('jadwal_matakuliah/tambah','jadwal_matakuliahController@tambah');
-
+ Route::get('jadwal_matakuliah','jadwal_matakuliahController@awal');
+ Route::get('jadwal_matakuliah/tambah','jadwal_matakuliahController@tambah');
+// Route::post('jadwal_matakuliah/simpan','jadwal_matakuliahController@simpan');
+// Route::get('jadwal_matakuliah/edit/{jadwal_matakuliah}','jadwal_matakuliahController@edit');
+// Route::post('jadwal_matakuliah/edit/{jadwal_matakuliah}','jadwal_matakuliahController@update');
+// Route::get('jadwal_matakuliah/hapus/{jadwal_matakuliah}','jadwal_matakuliahController@hapus');
 
